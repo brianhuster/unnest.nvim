af92e1daf3eeebc4ea4c4fe8980c5303ea32fddb
brianhuster <phambinhanctb2004@gmail.com>
2025-07-31 23:09:27 +0700

    add functional tests using pytest and pynvim
    Why do I use Pytest instead of Lua busted?
    - Easier to set up
    - pynvim has a lot of helper functions for controlling child Nvim

---
+++ test/test_plugin.py
@@ -0,0 +1,83 @@
+import pynvim
+import pytest
+import utils
+import time
+import os
+
+
+@pytest.fixture
+def nvim():
+    utils.init_env()
+
+    nvim = pynvim.attach(
+        'child', argv=["nvim", "--headless", "--embed"])
+    yield nvim
+    nvim.close()
+    utils.clean()
+
+
+def test_command(nvim):
+    """
+    Test command :UnnestEdit
+    """
+    winid = nvim.api.get_current_win()
+
+    nvim.command('UnnestEdit nvim Xtest/tmp/test_command.txt')
+    time.sleep(0.5)
+
+    # job must have been closed
+    job = nvim.current.window.vars['unnest_chan']
+    with pytest.raises(pynvim.api.common.NvimError) as err_info:
+        nvim.funcs.jobpid(job)
+    assert "Invalid channel id" in str(err_info.value)
+
+    # Don't change window after running command
+    assert winid == nvim.api.get_current_win()
+
+    buftype = nvim.current.buffer.options['buftype']
+    assert buftype == ''
+    bufname = nvim.current.buffer.name
+    assert bufname == os.path.join(
+        os.getcwd(), 'Xtest', 'tmp', 'test_command.txt')
+
+
+def test_winlayout(nvim):
+    """
+    Test winlayout when users use builtin terminal to open nested Nvim
+    """
+    tab = nvim.api.get_current_tabpage()
+
+    winbuflayout = ['col',
+                    [['row',
+                      [['leaf', {
+                          "name": os.path.abspath('README.md'),
+                          "diff": True}],
+                       ['leaf', {
+                           "name": os.path.abspath('LICENSE'),
+                           "diff": True}]]],
+                     ['leaf', {
+                         "name": os.path.abspath('.editorconfig'),
+                         "diff": False}]]]
+    nvim.command(
+        'term nvim -d "README.md" "LICENSE" +"botright split .editorconfig"')
+    time.sleep(0.5)
+
+    # Must be in a new tab
+    assert tab != nvim.api.get_current_tabpage()
+    assert nvim.funcs.tabpagenr() == 2
+
+    # the child Nvim hasn't been closed yet
+    child = nvim.current.tabpage.vars['unnest_socket']
+    assert nvim.funcs.sockconnect('pipe', child) != 0
+
+    # winlayout must be the same as in child Nvim
+    winlayout = nvim.funcs.winlayout()
+    assert utils.winlayout_handle_winid(nvim, winlayout) == winbuflayout
+
+    # Call :tabclose must close child Nvim, so sockconnect later must raise an
+    # error
+    nvim.command('tabclose')
+    time.sleep(0.1)
+    with pytest.raises(pynvim.api.common.NvimError) as connect_info:
+        nvim.funcs.sockconnect('pipe', child)
+    assert "connection failed: connection refused" in str(connect_info.value)
---
+++ test/utils.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import pynvim
+
+
+def init_env():
+    xdg_config_home = "Xtest/xdg/config"
+    xdg_data_home = "Xtest/xdg/share"
+    tmpdir = "Xtest/tmp"
+    os.environ["XDG_CONFIG_HOME"] = xdg_config_home
+    os.environ["XDG_DATA_HOME"] = xdg_data_home
+    os.environ["TMPDIR"] = tmpdir
+
+    # Unset $NVIM so that when you run tests inside Nvim terminal, it doesn't
+    # make parent Nvim open a new tab
+    if "NVIM" in os.environ:
+        del os.environ["NVIM"]
+
+    os.makedirs(tmpdir, exist_ok=True)
+    os.makedirs(xdg_config_home + "/nvim", exist_ok=True)
+
+    config_file = os.path.join(xdg_config_home, "nvim", "init.vim")
+    with open(config_file, "w") as f:
+        f.write("setl rtp+=.")
+
+
+def clean():
+    shutil.rmtree("Xtest", ignore_errors=True)
+
+
+def winlayout_handle_winid(nvim: pynvim.Nvim, winlayout):
+    """
+    Convert any winid in winlayout into is a dictionary like
+    {name: bufname, diff: &l:diff}
+    """
+    if isinstance(winlayout, list):
+        return [winlayout_handle_winid(nvim, w) for w in winlayout]
+    elif isinstance(winlayout, (int, float)):
+        return {
+            "name": nvim.api.buf_get_name(nvim.api.win_get_buf(winlayout)),
+            "diff": nvim.api.get_option_value("diff", {"win": winlayout})
+        }
+    else:
+        return winlayout
